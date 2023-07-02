c86c0ccdfbca257afe942011a9f0598bd0bbf59d
Aoyagibeautifulsea <summerhorse1123@outlook.jp>
2023-07-02 23:51:50 +0900

    [update] #1タイムゾーンの設定変更、マイグレーションファイルの作成#1タイムゾーンの変更、マイグレーションファイルの作成。

--- database/migrations/2023_07_02_232339_create_users_table.php
+++ database/migrations/2023_07_02_232339_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('adult_check');
+            $table->string('introduction', 150);
             $table->rememberToken();
             $table->timestamps();
         });
